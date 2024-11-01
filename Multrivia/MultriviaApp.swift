ccbe994496b64c43d1b7597cfb1839278d845dd9
Razvan Pricop <razvanipricop@gmail.com>
2024-11-01 10:31:15 +0100

    Initial Commit

---
+++ Multrivia/MultriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultriviaApp.swift
+//  Multrivia
+//
+//  Created by Razvan Pricop on 01.11.24.
+//
+
+import SwiftUI
+
+@main
+struct MultriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
