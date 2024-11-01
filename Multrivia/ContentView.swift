ca63f84804599ada6b1483feaae3c8d214c8ae7c
Razvan Pricop <razvanipricop@gmail.com>
2024-11-01 10:57:31 +0100

    Add question settings

--- Multrivia/ContentView.swift
+++ Multrivia/ContentView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedRange: Int = 2
+    @State private var numberOfQuestions: Int = 5
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Stepper("Up to: \(selectedRange)", value: $selectedRange, in: 2...12)
+            Stepper("Number of questions: \(numberOfQuestions)", value: $numberOfQuestions, in: 5...20, step: 5)
         }
-        .padding()
     }
 }
 
